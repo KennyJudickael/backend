6b4488c6e2253b91a4aafbb28df05289e9c613a6
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-05-06 17:11:42 +0300

    feat : route

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import userRouter from "./routes/user.routes.js";
+
 const app = express();
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
